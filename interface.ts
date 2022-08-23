25814194683cc2be0368e3dcab48a31323085349
UGEcko <vanhelsing314@outlook.com>
2022-08-23 13:57:16 -0400

    Create interface.ts

---
+++ interface.ts
@@ -0,0 +1,18 @@
+// Just examples of interfaces and what I can do with them.
+
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+
+const Rect: Rectangle = {
+    height: 10,
+    width: 20
+}
+
+const Squ: Rectangle = {
+    height: 10,
+    width: 10
+}
+
